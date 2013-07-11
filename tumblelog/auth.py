83803ef17ad1d5812e0b91cdc80ca969e17eaff8
Roman.Tsymbaliuk <mail.morro@gmail.com>
2013-07-11 21:37:16 +0300

    Added some changes into the project.

--- tumblelog/auth.py
+++ tumblelog/auth.py
@@ -6,7 +6,7 @@ def check_auth(username, password):
     """This function is called to check if a username /
     password combination is valid.
     """
-    return username == 'admin' and password == 'secret'
+    return username == 'admin' and password == 'secret1'
 
 
 def authenticate():
